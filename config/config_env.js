a86dc706ec03802ed381421a6385b8fa12ba21d7
Emen <skrubsquadproductions@gmail.com>
2018-11-07 10:58:19 -0500

    added production details in config file

--- config/config_env.js
+++ config/config_env.js
@@ -48,6 +48,14 @@ let config = {
             password: "Oseriemen20",
             dialect: "postgresql"
         },
+        stripe: {
+            publishable_key: 'pk_test_lBIB5BqfHoM4zbnte92tXRHP',
+            secret_key: 'sk_test_F2PuN5RhgQcJzk8vnOzQKmdg',
+            client_id: 'ca_DnDEKLGCeF2zZG1qpKFlFIqWvvj0sS10',
+            authorizeUri: 'https://connect.stripe.com/express/oauth/authorize',
+            tokenUri: 'https://connect.stripe.com/oauth/token',
+            redirect_uri: 'https://tenacitiportal.com/setup-payment-complete'
+        },
         server: {
             host: "127.0.0.1",
             port: 3001
