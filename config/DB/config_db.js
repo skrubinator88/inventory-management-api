571a69143e7eeefd03c9f1247125cf2964d25237
Emen <skrubsquadproductions@gmail.com>
2018-11-06 22:26:44 -0500

    changed db to force true to overwrite current db settings

--- config/DB/config_db.js
+++ config/DB/config_db.js
@@ -23,7 +23,7 @@ const db = new Sequelize(config.database.db_name, config.database.username, conf
 });
 
 //Connects db, forces all models to sync to test databases
-db.sync({force: false, match: /_test$/})
+db.sync({force: true, match: /_test$/})
 // db.sync({force: false})
     .then(() => {
         console.log("Database is successfuly connected");
