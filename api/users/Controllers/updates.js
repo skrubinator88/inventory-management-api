0c6624b2617f39b55708e97792375579b42d9fe6
Emen <skrubsquadproductions@gmail.com>
2018-12-01 12:39:29 -0500

    -added Promises to update user function

--- api/users/Controllers/updates.js
+++ api/users/Controllers/updates.js
@@ -5,27 +5,29 @@ const saltRounds = 5;
 
 module.exports = {
     async updateUserById (id, attributes, cb) {
-        let User = dbmain.model('User');
-        try {
-            if(attributes.password) {
-                let user = await User.findById(id);
-                if (user.password === attributes.password) {
-                    return cb(null, false)
-                } else {
-                    bcrypt.genSalt(saltRounds, (err, salt) => { //generate salt using saltRounds provided
-                        if (err) return cb(err);
-                        bcrypt.hash(attributes.password, salt, async (err, hash) => { //generate hash using password and salt generated
-                            console.log("Getting password encrypted...");
-                            attributes.password = hash;
+        return new Promise(async (resolve, reject) => {
+            let User = dbmain.model('User');
+            try {
+                if(attributes.password) {
+                    let user = await User.findById(id);
+                    if (user.password === attributes.password) {
+                        return resolve(false)
+                    } else {
+                        bcrypt.genSalt(saltRounds, (err, salt) => { //generate salt using saltRounds provided
+                            if(err) return reject(err);
+                                bcrypt.hash(attributes.password, salt, async (err, hash) => { //generate hash using password and salt generated
+                                    console.log("Getting password encrypted...");
+                                    attributes.password = hash;
+                                });
                         });
-                    });
+                    }
                 }
+                await User.update( attributes, { where: { id: id } });
+                return resolve(true)
+            } catch (err) {
+                return reject(err)
             }
-            await User.update( attributes, { where: { id: id } });
-            return cb(null, true)
-        } catch (err) {
-            return cb(err)
-        }
+        })
     },
     async updateProfilePhoto (id, imgLocation) {
         let Image = dbmain.model('Image');
