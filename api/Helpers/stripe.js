62d99e5638cc64ba601405e55b2182f1149ae161
Emen <skrubsquadproductions@gmail.com>
2019-01-26 19:13:43 -0500

    -added error handlers for stripe helper functions

--- api/Helpers/stripe.js
+++ api/Helpers/stripe.js
@@ -5,22 +5,30 @@ const stripe = require("stripe")(config.stripe.secret_key);
 
 module.exports = {
   async createToken(propertyAccountId, customerAccountId) {
-      return await stripe.tokens.create({
-          customer: customerAccountId,
-      }, {
-          stripe_account: propertyAccountId,
-      })
+      try {
+          return await stripe.tokens.create({
+              customer: customerAccountId,
+          }, {
+              stripe_account: propertyAccountId,
+          })
+      } catch (err) {
+          throw err
+      }
   },
     async chargePropertyCustomer(propertyAccountId, tokenId, chargeAmount) {
-        return await stripe.charges.create({
-            amount: chargeAmount,
-            currency: 'usd',
-            source: tokenId,
-            destination: {
-                amount: chargeAmount * .30,
-                account: propertyAccountId
-            }
-        });
+      try {
+          return await stripe.charges.create({
+              amount: chargeAmount,
+              currency: 'usd',
+              source: tokenId,
+              destination: {
+                  amount: chargeAmount * .30,
+                  account: propertyAccountId
+              }
+          });
+      } catch (err) {
+          throw err
+      }
     }
 
 };
